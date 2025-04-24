2a9dd802094c778034a9eb588dd61bd6ef1b7b59
Jane Buza <janebuza@Janes-MacBook-Air.local>
2025-04-24 08:28:52 -0400

    Level Two Complete

---
+++ levelTwo.py
@@ -0,0 +1,5 @@
+listOfStuff = [12, "Jane", True, 3.14159];
+playlistForCoding = ["Pink Diamond", "Britpop", "Crescent Sun", "UNISIL", "Anthems"];
+print(playlistForCoding[2]);
+playlistForCoding.append("Never Gonna Give You Up");
+# haha very funny
